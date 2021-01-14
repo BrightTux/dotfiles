cb088b8def567c15719e1011d497614c84f6f169
Clarence Cheong <clarence_han[at]hotmail[dot]com>
2021-01-14 21:11:48 +0800

    add fzf for zsh,bash

---
+++ .fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/tux/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/tux/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/tux/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/tux/.fzf/shell/key-bindings.bash"
