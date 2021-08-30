c0ccf45803be43ef60dda438bc7d1a1fc3e253a6
Clarence Cheong <clarence_han[at]hotmail[dot]com>
2021-08-30 16:41:44 +0800

    feat: uses $USER instead of hardcoding username

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/tux/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/tux/.fzf/bin"
+if [[ ! "$PATH" == */home/$USER/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/$USER/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/tux/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/$USER/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/tux/.fzf/shell/key-bindings.zsh"
+source "/home/$USER/.fzf/shell/key-bindings.zsh"
